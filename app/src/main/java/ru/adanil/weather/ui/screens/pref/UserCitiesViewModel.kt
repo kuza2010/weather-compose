b2038ce5cf6db38a7534285700c85e0864923783
Artyom Danilin <kyza20106@yandex.ru>
2022-06-19 14:36:53 +0300

    clean up mock data

--- app/src/main/java/ru/adanil/weather/ui/screens/pref/UserCitiesViewModel.kt
+++ app/src/main/java/ru/adanil/weather/ui/screens/pref/UserCitiesViewModel.kt
@@ -5,11 +5,11 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.updateAndGet
 import kotlinx.coroutines.launch
 import ru.adanil.weather.core.repository.CityRepository
 import ru.adanil.weather.model.City
-import kotlinx.coroutines.flow.collect
 import javax.inject.Inject
 
 data class UserCitiesUiState(
@@ -45,29 +45,6 @@ class UserCitiesViewModel @Inject constructor(
                     _uiState.value.copy(cities = userCity)
                 }
             }
-//            _uiState.update {
-//                it.copy(
-//                    cities = listOf(
-//                        City("Moscow_1", "Moscow_1", true),
-//                        City("Moscow_2", "Moscow_2", false),
-//                        City("Moscow_3", "Moscow_3", false),
-//                        City("Moscow_4", "Moscow_4", false),
-//                        City("Moscow_5", "Moscow_5", false),
-//                        City("Moscow_6", "Moscow_6", false),
-//                        City("Moscow_7", "Moscow_7", false),
-//                        City("Moscow_8", "Moscow_8", false),
-//                        City("Moscow_9", "Moscow_9", false),
-//                        City("Moscow_10", "Moscow_10", false),
-//                        City("Moscow_11", "Moscow_11", false),
-//                        City("Moscow_12", "Moscow_12", false),
-//                        City("Moscow_13", "Moscow_13", false),
-//                        City("Moscow_14", "Moscow_14", false),
-//                        City("Moscow_15", "Moscow_15", false),
-//                        City("Moscow_long_name_very_long_yes", "Moscow_long_name_very_long_yes", false),
-//                    ),
-//                    currentCity = City("Moscow_1","A", true)
-//                )
-//            }
         }
     }
 
