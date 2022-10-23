01687315266015be10401cb89d35d44f701a126b
Artyom Danilin <kyza20106@yandex.ru>
2022-10-23 21:14:01 +0300

    update AGP to 7.3.1

--- app/src/main/java/ru/adanil/weather/core/gateway/ForecastGateway.kt
+++ app/src/main/java/ru/adanil/weather/core/gateway/ForecastGateway.kt
@@ -3,7 +3,6 @@ package ru.adanil.weather.core.gateway
 import retrofit2.http.GET
 import retrofit2.http.Query
 import ru.adanil.weather.model.domain.TemperatureUnit
-import ru.adanil.weather.model.response.CurrentWeatherResponse
 import ru.adanil.weather.model.response.ForecastResponse
 
 interface ForecastGateway {
